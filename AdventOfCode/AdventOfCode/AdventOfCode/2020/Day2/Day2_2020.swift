10c138d9f65f8f531106de4fb95de494f55861cf
Chris Thomas <chris.r.thomas@johnlewis.co.uk>
2020-12-02 06:49:15 +0000

    Remove logging

--- AdventOfCode/AdventOfCode/AdventOfCode/2020/Day2/Day2_2020.swift
+++ AdventOfCode/AdventOfCode/AdventOfCode/2020/Day2/Day2_2020.swift
@@ -6,7 +6,6 @@ public func validSledPasswords(_ input: [Password: PasswordPolicy]) -> Int {
     
     input.forEach { (password, policy) in
         let count = password.password.count(of: policy.character)
-        print(count, policy.character, policy.min, policy.max)
         if count <= policy.max && count >= policy.min {
             validPasswords += 1
         }
@@ -23,7 +22,6 @@ public func validPasswords(_ input: [Password: PasswordPolicy]) -> Int {
     input.forEach { (password, policy) in
         let characterAtMin = password.password[policy.min-1..<policy.min]
         let characterAtMax = password.password[policy.max-1..<policy.max]
-        print(characterAtMin, characterAtMax, policy.character)
         if ((characterAtMin == String(policy.character) && characterAtMax != String(policy.character))) ||
             (characterAtMin != String(policy.character) && characterAtMax == String(policy.character)) {
             validPasswords += 1
