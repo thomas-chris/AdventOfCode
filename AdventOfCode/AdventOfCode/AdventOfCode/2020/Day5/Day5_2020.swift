dbd84759d64e0db8f80d911f6fe6cf9cdf0b9df1
Chris Thomas <chris.r.thomas@johnlewis.co.uk>
2020-12-05 08:31:07 +0000

    Day 5 2 ways

--- AdventOfCode/AdventOfCode/AdventOfCode/2020/Day5/Day5_2020.swift
+++ AdventOfCode/AdventOfCode/AdventOfCode/2020/Day5/Day5_2020.swift
@@ -7,5 +7,66 @@ extension TwentyTwenty {
 }
 
 extension TwentyTwenty.Day5 {
+    public func boardingPassFilter(input: [String]) -> ([(row: Int, column: Int, seat: Int)], Int, [Int]) {
+        
+        let seats = input.map { pass -> (row: Int, column: Int, seat: Int) in
+            var rows = 0...127
+            for value in pass.trimmingCharacters(in: .whitespacesAndNewlines).prefix(7) {
+                if value == "B" {
+                    rows = (rows.first! + rows.count / 2)...rows.last!
+                } else {
+                    rows = rows.first!...(rows.first! + rows.count / 2)
+                }
+            }
+            let row = rows.first!
+            
+            var columns = 0...7
+            for value in pass.trimmingCharacters(in: .whitespacesAndNewlines).suffix(3) {
+                if value == "R" {
+                    columns = (columns.first! + columns.count / 2)...columns.last!
+                } else {
+                    columns = columns.first!...(columns.first! + columns.count / 2)
+                }
+            }
+            let column = columns.first!
+            
+            return (row: row, column: column, seat: (8 * row) + column)
+        }
+        
+        let seatIds = seats.map { (row, column, seat) -> Int in seat }
+        
+        return (seats, seats.map { (row, column, seat) -> Int in seat }.max()!, seatIds)
+    }
     
+    public func boardingPassFilterV2(input: [String]) -> [Int] {
+        
+        return input.map { seat -> String in
+            seat.replacingOccurrences(of: "F", with: "0")
+                .replacingOccurrences(of: "B", with: "1")
+                .replacingOccurrences(of: "L", with: "0")
+                .replacingOccurrences(of: "R", with: "1")
+        }
+        .compactMap { seat -> Int? in
+            guard let row = Int(seat.prefix(7), radix: 2),
+                  let column = Int(seat.suffix(3), radix: 2) else {
+                return nil
+            }
+            
+            return 8 * row + column
+        }
+    }
+    
+    public func findYourPass(input: [Int]) -> Int {
+        let sorted = input.sorted { $0 < $1 }
+        var i = 0
+        var seat = sorted[0]
+        while true {
+            seat += 1
+            if sorted[i+1] != seat {
+                break
+            }
+            i += 1
+        }
+        return (sorted[i] + 1)
+    }
 }
--- AdventOfCode/AdventOfCode/AdventOfCodeTests/2020/Day5Tests2020.swift
+++ AdventOfCode/AdventOfCode/AdventOfCodeTests/2020/Day5Tests2020.swift
@@ -5,26 +5,56 @@ class Advent2020TestsDay5: XCTestCase {
 
     func test_day5_example1() {
         let input = Input.getInputStringNoTrim(name: "Day52020_Example1")
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+        let array = input.components(separatedBy: "\n")
         
-        
+        let boardingPassFilter = TwentyTwenty.Day5()
+        let results = boardingPassFilter.boardingPassFilter(input: array)
+        print(results)
+        XCTAssertEqual(results.1, 820)
     }
 
     func test_day5_part1() {
         let input = Input.getInputStringNoTrim(name: "Day52020")
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+        let array = input.components(separatedBy: "\n")
         
-        
+        let boardingPassFilter = TwentyTwenty.Day5()
+        let results = boardingPassFilter.boardingPassFilter(input: array)
+        XCTAssertEqual(results.1, 947)
         
     }
-
-    func test_day5_example2() {
-        let input = Input.getInputStringNoTrim(name: "Day52020_Example2")
+    
+    func test_day5_part1_v2() {
+        let input = Input.getInputStringNoTrim(name: "Day52020")
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+        let array = input.components(separatedBy: "\n")
         
-        
+        let boardingPassFilter = TwentyTwenty.Day5()
+        let result = boardingPassFilter.boardingPassFilterV2(input: array).max()
+        XCTAssertEqual(result, 947)
         
     }
 
     func test_day5_part2() {
         let input = Input.getInputString(name: "Day52020")
+        let array = input.components(separatedBy: "\n")
+        
+        let boardingPassFilter = TwentyTwenty.Day5()
+        let results = boardingPassFilter.boardingPassFilter(input: array)
+        let myPass = boardingPassFilter.findYourPass(input: results.2)
+        XCTAssertEqual(myPass, 636)
+
+    }
+    
+    func test_day5_part2_v2() {
+        let input = Input.getInputString(name: "Day52020")
+        let array = input.components(separatedBy: "\n")
+        
+        let boardingPassFilter = TwentyTwenty.Day5()
+        let results = boardingPassFilter.boardingPassFilterV2(input: array)
+        let myPass = boardingPassFilter.findYourPass(input: results)
+        XCTAssertEqual(myPass, 636)
 
     }
 }
