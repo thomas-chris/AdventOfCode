9dce425386e575974acd3814c2c4b9253823dbb5
Chris Thomas <chris.r.thomas@johnlewis.co.uk>
2020-12-12 06:24:10 +0000

    Day 12 part 2

--- AdventOfCode/AdventOfCode/AdventOfCode/2020/Day12/Day122020.swift
+++ AdventOfCode/AdventOfCode/AdventOfCode/2020/Day12/Day122020.swift
@@ -5,16 +5,141 @@ extension TwentyTwenty {
     public class Day12 {
         public init() {}
 
+        enum Instruction {
+            case forward(Int)
+            case move(Heading, Int)
+            case turn(Bool, Int)
+        }
+        
+        enum Heading: Int {
+            case north = 0
+            case east = 90
+            case south = 180
+            case west = 270
+        }
+        
     }
 }
 
-extension TwentyTwenty.Day11 {
-    public func solve1(input: [String]) {
+extension TwentyTwenty.Day12 {
+    public func solve1(input: [String]) -> Int {
+        let instructions = input.map { line -> Instruction in
+            let first = line.first!
+            let theRest = Int(line.dropFirst())!
+            
+            switch first {
+            case "N": return .move(.north, theRest)
+            case "E": return .move(.east, theRest)
+            case "S": return .move(.south, theRest)
+            case "W": return .move(.west, theRest)
+            case "L": return .turn(true, theRest)
+            case "R": return .turn(false, theRest)
+            case "F": return .forward(theRest)
+            default: fatalError("Invalid input")
+            }
+        }
+        
+        var position = Position(x: 0, y: 0)
+        var currentHeading: Heading = .east
+        
+        for instruction in instructions {
+            switch instruction {
+            case .forward(let distance) : position = position.move(heading: currentHeading, distance: distance)
+            case .move(let heading, let distance): position = position.move(heading: heading, distance: distance)
+            case .turn(let left, let angle): currentHeading = currentHeading.turn(left: left, angle: angle)
+            }
+            
+            print(position)
+        }
+
+        return (abs(position.x) + abs(position.y))
         
     }
     
-    public func solve2(input: [String]) {
+    public func solve2(input: [String]) -> Int {
+        let instructions = input.map { line -> Instruction in
+            let first = line.first!
+            let theRest = Int(line.dropFirst())!
+            
+            switch first {
+            case "N": return .move(.north, theRest)
+            case "E": return .move(.east, theRest)
+            case "S": return .move(.south, theRest)
+            case "W": return .move(.west, theRest)
+            case "L": return .turn(true, theRest)
+            case "R": return .turn(false, theRest)
+            case "F": return .forward(theRest)
+            default: fatalError("Invalid input")
+            }
+        }
         
+        var position = Position(x: 0, y: 0)
+        var waypoint = Position(x: 10, y: 1)
+        
+        for instruction in instructions {
+            switch instruction {
+            case .forward(let distance):
+                let x = waypoint.x * distance
+                let y = waypoint.y * distance
+                position = Position(x: position.x + x, y: position.y + y)
+            case .move(let heading, let distance): waypoint = waypoint.move(heading: heading, distance: distance)
+            case .turn(let left, let angle):
+                var times = (angle / 90) % 4
+                if left == false { times = 4 - times }
+                switch times {
+                case 0: continue
+                case 1: waypoint = Position(x: -waypoint.y, y: waypoint.x)
+                case 2: waypoint = Position(x: -waypoint.x, y: -waypoint.y)
+                case 3: waypoint = Position(x: waypoint.y, y:  -waypoint.x)
+                default: fatalError("Invalid input")
+                }
+            }
+
+        }
+        
+        return (abs(position.x) + abs(position.y))
+    }
+}
+
+extension Position {
+    func move(heading: TwentyTwenty.Day12.Heading, distance: Int) -> Position {
+        switch heading {
+        case .north : return Position(x: self.x, y: self.y + distance)
+        case .east : return Position(x: self.x + distance, y: self.y)
+        case .south : return Position(x: self.x, y: self.y - distance)
+        case .west : return Position(x: self.x - distance, y: self.y)
+        }
+    }
+}
+
+extension TwentyTwenty.Day12.Heading {
+    func turn(left: Bool, angle: Int) -> TwentyTwenty.Day12.Heading {
+        var heading = self
+        
+        let turns = angle / 90
+        
+        for _ in 1...turns {
+            if left {
+                switch heading {
+                case .north: heading = .west
+                case .east: heading = .north
+                case .south: heading = .east
+                case .west: heading = .south
+                }
+                
+            } else {
+                switch heading {
+                case .north: heading = .east
+                case .east: heading = .south
+                case .south: heading = .west
+                case .west: heading = .north
+                }
+            }
+            
+            
+        }
+        
+        return heading
     }
 }
 
--- AdventOfCode/AdventOfCode/AdventOfCodeTests/2020/Day12Tests2020.swift
+++ AdventOfCode/AdventOfCode/AdventOfCodeTests/2020/Day12Tests2020.swift
@@ -4,22 +4,34 @@ import AdventOfCode
 class Advent2020TestsDay12: XCTestCase {
     
     func test_day12_example1() {
-        
+        let input = Input.getInputString(name: "Day122020_Example1").components(separatedBy: "\n")
+        let day12 = TwentyTwenty.Day12()
+        let count = day12.solve1(input: input)
+        XCTAssertEqual(count, 25)
     }
     
     func test_day12() {
         measure {
-        
+            let input = Input.getInputString(name: "Day122020").components(separatedBy: "\n")
+            let day12 = TwentyTwenty.Day12()
+            let count = day12.solve1(input: input)
+            XCTAssertEqual(count, 415)
         }
     }
     
     func test_day12_example2() {
-        
+        let input = Input.getInputString(name: "Day122020_Example1").components(separatedBy: "\n")
+        let day12 = TwentyTwenty.Day12()
+        let count = day12.solve2(input: input)
+        XCTAssertEqual(count, 286)
     }
     
     func test_day12_part2() {
         measure {
-            
+            let input = Input.getInputString(name: "Day122020").components(separatedBy: "\n")
+            let day12 = TwentyTwenty.Day12()
+            let count = day12.solve2(input: input)
+            XCTAssertEqual(count, 415)
         }
     }
 }
