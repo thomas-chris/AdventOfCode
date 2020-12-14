63432d39a9be80281fc508c84bb13b05393cc06a
Chris Thomas <chris.r.thomas@johnlewis.co.uk>
2020-12-14 07:57:56 +0000

    Day 14 part 2

--- AdventOfCode/AdventOfCode/AdventOfCode/2020/Day14/Day142020.swift
+++ AdventOfCode/AdventOfCode/AdventOfCode/2020/Day14/Day142020.swift
@@ -7,7 +7,6 @@ extension TwentyTwenty {
         }
         
         
-        
         enum Instruction {
             case mask(zeros: [Int], ones: [Int])
             case memory(allocation: Int, value: Int)
@@ -26,6 +25,39 @@ extension TwentyTwenty {
                         }
                     }
                     self = .mask(zeros: zeros, ones: ones)
+                    return
+                }
+                
+                if string.contains("mem[") {
+                    let values = string.replacingOccurrences(of: "mem[", with: "").replacingOccurrences(of: "]", with: "").components(separatedBy: " = ")
+                    self = .memory(allocation: Int(values[0])!, value: Int(values[1])!)
+                    return
+                }
+                
+                fatalError()
+            }
+        }
+        
+        enum Instruction2 {
+            case mask(zeros: [Int], ones: [Int], exes: [Int])
+            case memory(allocation: Int, value: Int)
+            
+            init(string: String) {
+                if string.contains("mask =") {
+                    let value = string.replacingOccurrences(of: "mask = ", with: "")
+                    var zeros: [Int] = []
+                    var ones: [Int] = []
+                    var exes: [Int] = []
+                    
+                    value.enumerated().forEach { index, character in
+                        switch character {
+                        case "0": zeros.append(index)
+                        case "1": ones.append(index)
+                        case "X": exes.append(index)
+                        default: break
+                        }
+                    }
+                    self = .mask(zeros: zeros, ones: ones, exes: exes)
                     return
                 }
                 
@@ -90,13 +122,78 @@ extension TwentyTwenty.Day14 {
     }
     
     func pad(string : String, toSize: Int) -> String {
-      var padded = string
-      for _ in 0..<(toSize - string.count) {
-        padded = "0" + padded
-      }
-        return padded
+        String(repeating: "0", count: toSize) + string
     }
-    public func solvePart2(input: [String]) {
+    
+    public func solvePart2(input: [String]) -> Int {
+        let instructions = input.map { line in
+            return Instruction2(string: line)
+        }
         
+        var memory: [Int: Int] = [:]
+        
+        var zerosMask: [Int] = []
+        var onesMask: [Int] = []
+        var xMask: [Int] = []
+        
+        instructions.forEach { instruction in
+            
+            if case .mask(let zeros, let ones, let exes) = instruction {
+                zerosMask = zeros
+                onesMask = ones
+                xMask = exes
+            }
+            
+            if case .memory(let allocation, value: let value) = instruction {
+                let allocationAsBinary = String(allocation, radix: 2)
+                let paddedValue = String(repeating: "0", count: 36 - allocationAsBinary.count) + allocationAsBinary
+                
+                let bothSwapped = paddedValue.enumerated().map { index, character -> String in
+                    if onesMask.contains(index) {
+                        return "1"
+                    } else {
+                        return String(character)
+                    }
+                    
+                }.reduce("", +)
+                
+                let xSwapped = bothSwapped.enumerated().map { index, character -> String in
+                    if xMask.contains(index) {
+                        return "X"
+                    } else {
+                        return String(character)
+                    }
+                }.reduce("", +)
+                
+                let memoryAllocation = getAllValues(string: xSwapped, exes: xMask)
+                
+                for i in memoryAllocation {
+                    memory[i] = value
+                }
+            }
+        }
+        
+        return memory.values.reduce(0, +)
+    }
+    
+    func getAllValues(string: String, exes: [Int]) -> [Int] {
+        var values: [Int] = []
+        let base = Int(string.replacingOccurrences(of: "X", with: "0"), radix: 2) ?? 0
+        let iterations = Int("1" + String(repeating: "0", count: exes.count), radix: 2) ?? 0
+                
+        
+        for i in 0..<iterations {
+            let number = String(i, radix: 2)
+            let binary = pad(string: String(i, radix: 2), toSize: exes.count - number.count) + number
+            
+            var result = Array(repeating: "0", count: 36)
+            
+            zip(binary, exes).forEach { (character, index) in
+                result[index] = String(character)
+            }
+            let value = Int(result.joined(), radix: 2) ?? 0
+            values.append(base + value)
+        }
+        return values
     }
 }
--- AdventOfCode/AdventOfCode/AdventOfCodeTests/2020/Day14Tests2020.swift
+++ AdventOfCode/AdventOfCode/AdventOfCodeTests/2020/Day14Tests2020.swift
@@ -18,4 +18,35 @@ class Advent2020TestsDay14: XCTestCase {
         XCTAssertEqual(value, 15172047086292)
 
     }
+
+    func test_day14_part2_example1() {
+        let input = Input.getInputString(name: "Day142020_Example1").components(separatedBy: "\n")
+        let day14 = TwentyTwenty.Day14()
+        let value = day14.solvePart2(input: input)
+        XCTAssertEqual(value, 165)
+
+    }
+    
+    func test_part2_examplePJ() {
+            let input = """
+            mask = 000000000000000000000000000000X1001X
+            mem[42] = 100
+            mask = 00000000000000000000000000000000X0XX
+            mem[26] = 1
+            """.components(separatedBy: "\n")
+        let day14 = TwentyTwenty.Day14()
+        let value = day14.solvePart2(input: input)
+        XCTAssertEqual(value, 208)
+        }
+    
+    func test_day14_part2() {
+        let input = Input.getInputString(name: "Day142020").components(separatedBy: "\n")
+        let day14 = TwentyTwenty.Day14()
+        let value = day14.solvePart2(input: input)
+        XCTAssertEqual(value, 4197941339968)
+
+    }
+
+
+
 }
