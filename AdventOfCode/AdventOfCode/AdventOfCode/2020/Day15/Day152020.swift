77631a3996a21e853d87dd78038561f8f10b8fea
Chris Thomas <chris.r.thomas@johnlewis.co.uk>
2020-12-15 08:32:32 +0000

    Day 15

--- AdventOfCode/AdventOfCode/AdventOfCode/2020/Day15/Day152020.swift
+++ AdventOfCode/AdventOfCode/AdventOfCode/2020/Day15/Day152020.swift
@@ -5,17 +5,69 @@ extension TwentyTwenty {
     public class Day15 {
         public init() {
         }
-    
+        
+        struct TurnAndSpoken {
+            let lastSpokenValue: Int
+            let previousLastSpokenValue: Int
+        }
     }
     
 }
 
 extension TwentyTwenty.Day15 {
-    public func solvePart1(input: [String]) {
+    public func solvePart1(input: [Int]) -> Int {
+        let times = 2020
+        var turnForNumber: [Int: TurnAndSpoken] = [:]
+        var turn = 0
         
+        let defaultValue = TurnAndSpoken(lastSpokenValue: -1, previousLastSpokenValue: -1)
+        
+        var lastNumberSpoken = 0
+        for value in input {
+            turn += 1
+            turnForNumber[value] = TurnAndSpoken(lastSpokenValue: turn, previousLastSpokenValue: -1)
+            lastNumberSpoken = value
+        }
+        
+        while turn < times {
+            turn += 1
+            let value = turnForNumber[lastNumberSpoken] ?? defaultValue
+            if value.lastSpokenValue >= 0 && value.previousLastSpokenValue >= 0 {
+                lastNumberSpoken = value.lastSpokenValue - value.previousLastSpokenValue
+            } else {
+                lastNumberSpoken = 0
+            }
+            let newValue = turnForNumber[lastNumberSpoken] ?? defaultValue
+            turnForNumber[lastNumberSpoken] = TurnAndSpoken(lastSpokenValue: turn, previousLastSpokenValue: newValue.lastSpokenValue)
+        }
+        return lastNumberSpoken
     }
     
-    public func solvePart2(input: [String]) {
+    public func solvePart2(input: [Int]) -> Int {
+        let times = 30000000
+        var turnForNumber: [Int: TurnAndSpoken] = [:]
+        var turn = 0
         
+        let defaultValue = TurnAndSpoken(lastSpokenValue: -1, previousLastSpokenValue: -1)
+        
+        var lastNumberSpoken = 0
+        for value in input {
+            turn += 1
+            turnForNumber[value] = TurnAndSpoken(lastSpokenValue: turn, previousLastSpokenValue: -1)
+            lastNumberSpoken = value
+        }
+        
+        while turn < times {
+            turn += 1
+            let value = turnForNumber[lastNumberSpoken] ?? defaultValue
+            if value.lastSpokenValue >= 0 && value.previousLastSpokenValue >= 0 {
+                lastNumberSpoken = value.lastSpokenValue - value.previousLastSpokenValue
+            } else {
+                lastNumberSpoken = 0
+            }
+            let newValue = turnForNumber[lastNumberSpoken] ?? defaultValue
+            turnForNumber[lastNumberSpoken] = TurnAndSpoken(lastSpokenValue: turn, previousLastSpokenValue: newValue.lastSpokenValue)
+        }
+        return lastNumberSpoken
     }
 }
--- AdventOfCode/AdventOfCode/AdventOfCodeTests/2020/Day15Tests2020.swift
+++ AdventOfCode/AdventOfCode/AdventOfCodeTests/2020/Day15Tests2020.swift
@@ -3,20 +3,45 @@ import AdventOfCode
 
 class Advent2020TestsDay15: XCTestCase {
     
-    func test_day14_example1() {
-        let input = Input.getInputString(name: "Day152020_Example1").components(separatedBy: "\n")
+    func test_day15_example0() {
+        let input = [0,3,6]
         let day15 = TwentyTwenty.Day15()
         let value = day15.solvePart1(input: input)
-//        XCTAssertEqual(value, 165)
+        XCTAssertEqual(value, 436)
+    }
+    
+    func test_day15_example1() {
+        let input = [1,3,2]
+        let day15 = TwentyTwenty.Day15()
+        let value = day15.solvePart1(input: input)
+        XCTAssertEqual(value, 1)
+
+    }
+    
+    func test_day15_example2() {
+        let input = [2,1,3]
+        let day15 = TwentyTwenty.Day15()
+        let value = day15.solvePart1(input: input)
+        XCTAssertEqual(value, 10)
 
     }
     
     func test_day14_part1() {
         measure {
-        let input = Input.getInputString(name: "Day152020").components(separatedBy: "\n")
+        let input = [16,1,0,18,12,14,19]
         let day15 = TwentyTwenty.Day15()
         let value = day15.solvePart1(input: input)
-//        XCTAssertEqual(value, 15172047086292)
+       XCTAssertEqual(value, 929)
+        }
+
+    }
+    
+    func test_day14_part2() {
+        measure {
+        let input = [16,1,0,18,12,14,19]
+        let day15 = TwentyTwenty.Day15()
+        let value = day15.solvePart2(input: input)
+       XCTAssertEqual(value, 16671510)
         }
 
     }
