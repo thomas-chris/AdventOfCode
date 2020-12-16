3e3a86e1855a42900129d25a0874f1e2e43170fc
Chris <chris.r.thomas@johnlewis.co.uk>
2020-12-16 06:43:45 +0000

    Please don't look at this - day 16

--- AdventOfCode/AdventOfCode/AdventOfCode/2020/Day16/Day162020.swift
+++ AdventOfCode/AdventOfCode/AdventOfCode/2020/Day16/Day162020.swift
@@ -11,13 +11,169 @@ extension TwentyTwenty {
 }
 
 extension TwentyTwenty.Day16 {
-    public func solvePart1(input: [String]) -> Int {
+    public func solvePart1(input: String) -> Int {
         
-        return 0
+        var rulesSet = Set<Int>()
+        var nearbyTickets = Array<Int>()
+        
+        let blocks = input.components(separatedBy: "\n\n")
+        
+        let notes = blocks.first!
+        let otherTickets = blocks.last!
+        
+        let noteLines = notes.components(separatedBy: "\n")
+        
+        for line in noteLines {
+            
+            let rangesOfNotes = line.components(separatedBy: ":")[1].trimmingCharacters(in: .whitespaces).components(separatedBy: " or ")
+            for value in rangesOfNotes {
+                let startEnd = value.components(separatedBy: "-")
+                for i in Array(Int(startEnd[0])! ... Int(startEnd[1])!) {
+                    rulesSet.insert(i)
+                }
+            }
+        }
+        
+        
+        otherTickets.components(separatedBy: ":").last!.components(separatedBy: "\n").forEach { line in
+            line.components(separatedBy: ",")
+                .compactMap {
+                    if $0.count > 0 {
+                        return Int($0)!
+                    }
+                    return nil
+                }
+                .forEach { value in
+                    nearbyTickets.append(value)
+                }
+        }
+        
+        var errors: [Int] = []
+        var validOtherTickets = [Int]()
+        for i in nearbyTickets {
+            if !rulesSet.contains(i) {
+                errors.append(i)
+            } else {
+                validOtherTickets.append(i)
+            }
+        }
+        return errors.reduce(0, +)
     }
     
-    public func solvePart2(input: [String]) -> Int {
-       
-        return 0
+    public func solvePart2(input: String) -> Int {
+        
+        var allRules = Set<Int>()
+        var rulesSets = Array<Set<Int>>()
+        let blocks = input.components(separatedBy: "\n\n")
+        
+        let notes = blocks.first!
+        let otherTickets = blocks.last!
+        let myTicket = blocks[1]
+        
+        let noteLines = notes.components(separatedBy: "\n")
+        
+        for line in noteLines {
+            var set = Set<Int>()
+            
+            let rangesOfNotes = line.components(separatedBy: ":")[1].trimmingCharacters(in: .whitespaces).components(separatedBy: " or ")
+            for value in rangesOfNotes {
+                let startEnd = value.components(separatedBy: "-")
+                for i in Array(Int(startEnd[0])! ... Int(startEnd[1])!) {
+                    set.insert(i)
+                    allRules.insert(i)
+                }
+            }
+            rulesSets.append(set)
+        }
+        
+        var validOtherTickets: [[Int]] = []
+        otherTickets.components(separatedBy: ":").last!.components(separatedBy: "\n").forEach { line in
+            if line.count > 0 {
+                let values = line.components(separatedBy: ",").compactMap { Int($0) }
+                var valid = true
+                for i in values {
+                    if !allRules.contains(i) {
+                        valid = false
+                    }
+                }
+                
+                if valid {
+                    validOtherTickets.append(values)
+                }
+            }
+            
+        }
+        
+        var dictionary: [Int: [Int]] = [:]
+        
+        for i in validOtherTickets {
+            for j in 0..<i.count {
+                let list = dictionary[j] ?? []
+                dictionary[j] = list + [i[j]]
+            }
+        }
+        
+        var possibleResults: [Int: [Int]] = [:]
+        dictionary.forEach { (position, values) in
+            for i in 0..<rulesSets.count {
+                let rules = rulesSets[i]
+                var valid = true
+                for j in values {
+                    if !rules.contains(j) {
+                        valid = false
+                    }
+                }
+                
+                if valid {
+                    let list = possibleResults[position] ?? []
+                    possibleResults[position] = list + [i]
+                }
+            }
+        }
+        
+        var columns: [Int: Int] = [:]
+        while !possibleResults.isEmpty {
+            (possibleResults, columns) = getRuleWith1(values: possibleResults, columns: columns)
+        }
+        
+        let myTicketValues = myTicket.components(separatedBy: ":\n").last!.components(separatedBy: ",").compactMap { Int($0) }
+        
+        let indexs = columns.compactMap { (key, value) -> Int? in
+            if key < 6 {
+                return value
+            }
+            return nil
+        }
+
+        var finally = [Int]()
+        indexs.forEach { (index) in
+            finally.append(myTicketValues[index])
+        }
+        return finally.reduce(1, *)
+    }
+    
+    func getRuleWith1(values: [Int: [Int]], columns: [Int: Int]) -> ([Int: [Int]], [Int: Int]) {
+        var value = -1
+        var columns = columns
+        values.forEach { (key, list) in
+            if list.count == 1 {
+                value = list.first!
+                columns[value] = key
+                print("Column \(value) is rule \(key)")
+            }
+        }
+        
+        var newValues: [Int: [Int]] = [:]
+        
+        values.forEach { (key, list) in
+            let newList = list.filter { (number) -> Bool in
+                number != value
+            }
+            if newList.count > 0 {
+                newValues[key] = newList
+            }
+        }
+        
+        return (newValues, columns)
     }
 }
--- AdventOfCode/AdventOfCode/AdventOfCodeTests/2020/Day16Tests2020.swift
+++ AdventOfCode/AdventOfCode/AdventOfCodeTests/2020/Day16Tests2020.swift
@@ -4,31 +4,37 @@ import AdventOfCode
 class Advent2020TestsDay16: XCTestCase {
     
     func test_day16_example1() {
-        let input = Input.getInputString(name: "Day162020_Example1").components(separatedBy: "\n")
+        let input = Input.getInputString(name: "Day162020_Example1")
         let day16 = TwentyTwenty.Day16()
         let value = day16.solvePart1(input: input)
-        XCTAssertEqual(value, 165)
+        XCTAssertEqual(value, 71)
 
     }
     
     func test_day16_part1() {
-        measure {
-        let input = Input.getInputString(name: "Day162020").components(separatedBy: "\n")
+//        measure {
+        let input = Input.getInputString(name: "Day162020")
         let day16 = TwentyTwenty.Day16()
         let value = day16.solvePart1(input: input)
-        XCTAssertEqual(value, 15172047086292)
-        }
+        XCTAssertEqual(value, 19240)
+//        }
 
     }
     
-    func test_day16_part2() {
-        measure {
-            
-        let input = Input.getInputString(name: "Day162020").components(separatedBy: "\n")
+    func test_day16_part2_example() {
+
+        let input = Input.getInputString(name: "Day162020_Example1")
         let day16 = TwentyTwenty.Day16()
         let value = day16.solvePart2(input: input)
-        XCTAssertEqual(value, 4197941339968)
-        }
+        XCTAssertEqual(value, 1716)
+    }
+    
+    func test_day16_part2() {
+
+        let input = Input.getInputString(name: "Day162020")
+        let day16 = TwentyTwenty.Day16()
+        let value = day16.solvePart2(input: input)
+        XCTAssertEqual(value, 5527728694937)
     }
 
 
