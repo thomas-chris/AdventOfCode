6a79813651f6243f5ecf63e74cc57fe06c789b04
Chris <chris.r.thomas@johnlewis.co.uk>
2020-12-18 06:31:55 +0000

    Day 18

---
+++ AdventOfCode/AdventOfCode/AdventOfCode/2020/Day18/Day182020.swift
@@ -0,0 +1,114 @@
+import Foundation
+import Algorithms
+
+extension TwentyTwenty {
+    public class Day18 {
+        public init() {
+        }
+        
+        let operators: [String:(Int,Int)->Int] = [
+            "+": (+),
+            "-": (-),
+            "/": (/),
+            "*": (*),
+        ]
+    }
+    
+    
+}
+
+extension TwentyTwenty.Day18 {
+    public func solvePart1(input: [String]) -> Int {
+        
+        var sums = [Int]()
+        
+        for line in input {
+            let last = breakUpLine(string: line, solve: solveBlock)
+            let result = solveBlock(string: last)
+            sums.append(result)
+        }
+        
+        return(sums.reduce(0, +))
+    }
+    
+    public func solvePart2(input: [String]) -> Int {
+        var sums = [Int]()
+        
+        for line in input {
+            let last = breakUpLine(string: line, solve: solveBlock2)
+            let result = solveBlock2(string: last)
+            sums.append(result)
+        }
+        
+        return(sums.reduce(0, +))
+    }
+    
+    private func breakUpLine(string: String, solve: (String) -> Int) -> String {
+        
+        var smallestBlock = ""
+        var newString = string
+        
+        for character in string {
+            if character == "(" {
+                smallestBlock = ""
+            } else if character == ")" {
+                let replacement = solve(smallestBlock)
+                newString = string.replacingOccurrences(of: "(\(smallestBlock))", with: String(replacement))
+                newString = breakUpLine(string: newString, solve: solve)
+                return newString
+            } else {
+                smallestBlock += String(character)
+            }
+        }
+        
+        return newString
+    }
+    
+    private func solveBlock(string: String) -> Int {
+        
+        let characters = string.components(separatedBy: " ")
+        var lastInt = 0
+        var useOperator: ((Int, Int) -> Int)? = nil
+        
+        for character in characters {
+            if let int = Int(character) {
+                if useOperator == nil {
+                    lastInt = int
+                } else {
+                    lastInt = useOperator!(lastInt, int)
+                    useOperator = nil
+                }
+            } else if ["*", "+", "/", "-"].contains(character) {
+                useOperator = operators[character]
+            }
+        }
+        
+        return lastInt
+    }
+    
+    private func solveBlock2(string: String) -> Int {
+        let sums = string.components(separatedBy: " * ")
+        var thingsToMultiply = [Int]()
+        for list in sums {
+            let chunks = list.components(separatedBy: " ")
+            var lastInt = 0
+            var useOperator: ((Int, Int) -> Int)? = nil
+            
+            for character in chunks {
+                if let int = Int(String(character)) {
+                    if useOperator == nil {
+                        lastInt = int
+                    } else {
+                        lastInt = useOperator!(lastInt, int)
+                        useOperator = nil
+                    }
+                } else if ["*", "+", "/", "-"].contains(character) {
+                    useOperator = operators[String(character)]
+                }
+            }
+            thingsToMultiply.append(lastInt)
+        }
+        
+        return thingsToMultiply.reduce(1, *)
+    }
+}
---
+++ AdventOfCode/AdventOfCode/AdventOfCodeTests/2020/Day18Tests2020.swift
@@ -0,0 +1,111 @@
+import XCTest
+import AdventOfCode
+
+class Advent2020TestsDay18: XCTestCase {
+    
+    func test_day18_example1() {
+        let input = ["1 + 2 * 3 + 4 * 5 + 6"]
+        let day18 = TwentyTwenty.Day18()
+        let value = day18.solvePart1(input: input)
+        XCTAssertEqual(value, 71)
+
+    }
+    
+    func test_day18_example2() {
+        let input = ["1 + (2 * 3) + (4 * (5 + 6))"]
+        let day18 = TwentyTwenty.Day18()
+        let value = day18.solvePart1(input: input)
+        XCTAssertEqual(value, 51)
+
+    }
+    
+    func test_day18_example3() {
+        let input = [
+            "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"
+        ]
+        let day18 = TwentyTwenty.Day18()
+        let value = day18.solvePart1(input: input)
+        XCTAssertEqual(value, 13632)
+
+    }
+    
+    func test_day18_example4() {
+        let input = [
+            "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"
+        ]
+        let day18 = TwentyTwenty.Day18()
+        let value = day18.solvePart1(input: input)
+        XCTAssertEqual(value, 12240)
+
+    }
+    
+    func test_day18_example5() {
+        let input = [
+            "2 * 3 + (4 * 5)",
+            "5 + (8 * 3 + 9 + 3 * 4 * 3)",
+            "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))",
+            "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"
+        ]
+        let day18 = TwentyTwenty.Day18()
+        let value = day18.solvePart1(input: input)
+        XCTAssertEqual(value, 26335)
+
+    }
+    
+    func test_day18_part1() {
+        let input = Input.getInputString(name: "Day182020").components(separatedBy: "\n")
+        let day18 = TwentyTwenty.Day18()
+        let value = day18.solvePart1(input: input)
+        XCTAssertEqual(value, 7293529867931)
+    }
+    
+    func test_day18_2_example1() {
+        let input = ["1 + 2 * 3 + 4 * 5 + 6"]
+        let day18 = TwentyTwenty.Day18()
+        let value = day18.solvePart2(input: input)
+        XCTAssertEqual(value, 231)
+
+    }
+    
+    func test_day18_2_example2() {
+        let input = ["1 + (2 * 3) + (4 * (5 + 6))"]
+        let day18 = TwentyTwenty.Day18()
+        let value = day18.solvePart2(input: input)
+        XCTAssertEqual(value, 51)
+
+    }
+    
+    func test_day18_2_example3() {
+        let input = [
+            "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"
+        ]
+        let day18 = TwentyTwenty.Day18()
+        let value = day18.solvePart2(input: input)
+        XCTAssertEqual(value, 13632)
+
+    }
+    
+    func test_day18_2_example4() {
+        let input = [
+            "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"
+        ]
+        let day18 = TwentyTwenty.Day18()
+        let value = day18.solvePart2(input: input)
+        XCTAssertEqual(value, 12240)
+
+    }
+    
+    
+    func test_day18_part2() {
+        measure {
+            
+        let input = Input.getInputString(name: "Day182020").components(separatedBy: "\n")
+        let day18 = TwentyTwenty.Day18()
+        let value = day18.solvePart2(input: input)
+        XCTAssertEqual(value, 275183)
+        }
+    }
+
+
+
+}
