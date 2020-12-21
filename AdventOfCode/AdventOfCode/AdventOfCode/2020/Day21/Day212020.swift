9eb726820d1ee374e10c0566296fb9412ee67765
Chris <chris.r.thomas@johnlewis.co.uk>
2020-12-21 10:32:23 +0000

    Day 21

--- AdventOfCode/AdventOfCode/AdventOfCode/2020/Day21/Day212020.swift
+++ AdventOfCode/AdventOfCode/AdventOfCode/2020/Day21/Day212020.swift
@@ -1,9 +1,74 @@
-//
-//  Day212020.swift
-//  AdventOfCode
-//
-//  Created by Chris Thomas on 21/12/2020.
-//  Copyright © 2020 Chris Thomas. All rights reserved.
-//
+import Algorithms
 
-import Foundation
+extension TwentyTwenty {
+    public class Day21 {
+        public init() {
+        }
+    }
+}
+
+extension TwentyTwenty.Day21 {
+    public func solvePart1(input: [String]) -> Int {
+        
+        let (ingridientCount, allergensTable) = getIngridientsCountAndAllergens(input: input)
+       
+        let possibleAllergenicIngredients = allergensTable.values.flatMap({ $0 })
+        
+        var newIngCount = [String: Int]()
+        for ingridient in ingridientCount.keys {
+            if !possibleAllergenicIngredients.contains(ingridient) {
+                newIngCount[ingridient] = ingridientCount[ingridient]!
+            }
+        }
+
+        return newIngCount.values.reduce(0, +)
+    }
+    
+    public func solvePart2(input: [String]) -> String {
+        
+        var (_, allergensTable) = getIngridientsCountAndAllergens(input: input) 
+        
+        while allergensTable.values.contains(where: { $0.count > 1 }) {
+            allergensTable.forEach { (allergen, ingridients) in
+                if ingridients.count == 1 {
+                    allergensTable.forEach { (key, _) in
+                        if key != allergen {
+                            allergensTable[key]?.remove(ingridients.first!)
+                        }
+                    }
+                }
+            }
+        }
+        
+        return allergensTable.keys
+            .sorted()
+            .compactMap { allergensTable[$0]?.first }
+            .joined(separator: ",")
+    }
+    
+    private func getIngridientsCountAndAllergens(input: [String]) -> ([String: Int], [String: Set<String>]) {
+        var ingridientCount = [String: Int]()
+        var allergensTable = [String: Set<String>]()
+        
+        for line in input {
+            let splitLine = line.components(separatedBy: "(contains ")
+            let ingridients = splitLine[0]
+                .components(separatedBy: .whitespaces).filter { $0 != "" }
+            let allergens = Set(splitLine[1].replacingOccurrences(of: ")", with: "").components(separatedBy: ", "))
+            
+            for i in ingridients {
+                ingridientCount[i, default: 0] += 1
+            }
+            
+            for allergen in allergens {
+                if let existingAllergen = allergensTable[allergen] {
+                    allergensTable[allergen] = existingAllergen.intersection(ingridients)
+                } else {
+                    allergensTable[allergen] = Set(ingridients)
+                }
+            }
+        }
+        
+        return (ingridientCount, allergensTable)
+    }
+}
--- AdventOfCode/AdventOfCode/AdventOfCodeTests/2020/Day21Tests2020.swift
+++ AdventOfCode/AdventOfCode/AdventOfCodeTests/2020/Day21Tests2020.swift
@@ -1,9 +1,23 @@
-//
-//  Day21Tests2020.swift
-//  AdventOfCodeTests
-//
-//  Created by Chris Thomas on 21/12/2020.
-//  Copyright © 2020 Chris Thomas. All rights reserved.
-//
+import XCTest
+import AdventOfCode
 
-import Foundation
+class Advent2020TestsDay21: XCTestCase {
+    
+    func test_day21_part1() {
+        measure {
+            let input = Input.getInputString(name: "Day212020").components(separatedBy: "\n")
+            let day21 = TwentyTwenty.Day21()
+            let value = day21.solvePart1(input: input)
+            XCTAssertEqual(value, 2098)
+        }
+    }
+    
+    func test_day21_part2() {
+        measure {
+            let input = Input.getInputString(name: "Day212020").components(separatedBy: "\n")
+            let day21 = TwentyTwenty.Day21()
+            let value = day21.solvePart2(input: input)
+            XCTAssertEqual(value, "ppdplc,gkcplx,ktlh,msfmt,dqsbql,mvqkdj,ggsz,hbhsx")
+        }
+    }
+}
