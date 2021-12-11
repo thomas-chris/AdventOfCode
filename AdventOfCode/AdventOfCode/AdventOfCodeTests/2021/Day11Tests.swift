cb8c288024bd198d353525d11136c81f2d6f81d1
Chris Thomas <chris.r.thomas@johnlewis.co.uk>
2021-12-11 09:23:26 +0000

    Day 11

---
+++ AdventOfCode/AdventOfCode/AdventOfCodeTests/2021/Day11Tests.swift
@@ -0,0 +1,28 @@
+import XCTest
+import TwentyTwentyOne
+
+class TestsDay11: XCTestCase {
+
+    let input = Array(Input.array(seperator: "\n", file: "Day11", compactmap:  { string in string } ).dropLast())
+        
+    let example = Array(Input.array(seperator: "\n", file: "Day11Example", compactmap:  { string in string } ).dropLast())
+    
+    func test_day11_part1_example() {
+        XCTAssertEqual(Day11.part1(example, turns: 100), 1656)
+    }
+    
+    func test_day11_part1() {
+        XCTAssertEqual(Day11.part1(input, turns: 100), 1649)
+    }
+    
+    func test_day11_example() throws {
+        XCTAssertEqual(try Day11.part2(example), 195)
+    }
+    
+    func test_day11_part2() throws {
+        measure {
+            XCTAssertEqual(try? Day11.part2(input), 256)
+        }
+    }
+}
+
