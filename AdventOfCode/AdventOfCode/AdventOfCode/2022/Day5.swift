3c3329ad156a595a19c1afcdf3468bc81e110721
Chris Thomas <chris.r.thomas@johnlewis.co.uk>
2022-12-05 11:29:04 +0000

    Day 5

--- AdventOfCode/AdventOfCode/AdventOfCode/2022/Day5.swift
+++ AdventOfCode/AdventOfCode/AdventOfCode/2022/Day5.swift
@@ -5,49 +5,104 @@ import UIKit
 
 public struct Day5 {
     
-    public static func part1(_ input: [String]) -> Int {
-        solve(input, filter: true)
+    public typealias Stack = [Character]
+    public typealias Stacks = [Int: Stack]
+    static let regex = Regex("[\\w]* ([\\d]+) [\\w]* ([\\d]+) [\\w]* ([\\d]+)")
+    
+    public static func part1(_ input: [String]) -> String {
+        
+        var (map, instructions) = parse(input)
+        for instruction in instructions {
+            for i in 1...instruction[0] {
+                var from = map[instruction[1]]
+                var to = map[instruction[2]]
+                let last = from?.removeLast() ?? " "
+                to?.append(last)
+                
+                map[instruction[2]] = to
+                map[instruction[1]] = from
+            }
+        }
+        
+        var solution = ""
+        for key in 1...map.keys.count {
+            solution.append(map[key]?.last ?? Character(""))
+        }
+        return solution
     }
     
-    public static func part2(_ input: [String]) throws -> Int {
-        solve(input, filter: false)
+    public static func part2(_ input: [String]) throws -> String {
+        var (map, instructions) = parse(input)
+        
+        for instruction in instructions {
+            guard var from = map[instruction[1]],
+                  var to = map[instruction[2]]
+            else { fatalError("failure to parse data") }
+            
+            let move = from.dropFirst(((from.count ?? 0) - instruction[0]))
+            to.append(contentsOf: move)
+            
+            map[instruction[2]] = to
+            map[instruction[1]] = from.dropLast(instruction[0])
+            
+        }
+        
+        var solution = ""
+        for key in 1...map.keys.count {
+            solution.append(map[key]?.last ?? Character(" "))
+        }
+        return solution
     }
     
-    public struct Vent {
-        let start: Position
-        let end: Position
-    }
-    
-    private static func getVents(_ input: [String], filterDiagonals: Bool) -> [Vent] {
-        let regex: Regex = Regex(#"(\d+),(\d+) -> (\d+),(\d+)"#)
-        let vents = input
-            .map { line -> Vent in
-                let match = regex.firstMatch(in: line)!.captures.compactMap { $0 }
-                return Vent(start: Position(x: Int(match[0])!,
-                                            y: Int(match[1])!),
-                            end: Position(x: Int(match[2])!,
-                                          y: Int(match[3])!))
+    private static func parse(_ input: [String]) -> ([Optional<Int> : Day5.Stack], [[Int]]) {
+        guard let mapValues = input
+                .first?
+                .components(separatedBy: "\n")
+                .map(Array.init),
+            let instructions = input
+                .last?
+                .components(separatedBy: "\n")
+                .dropLast()
+                .compactMap({ string in
+                    regex.firstMatch(in: string)?.captures.compactMap({ string in
+                        Int(string ?? "")
+                    })
+                })
+        else { fatalError("Failed to parse data") }
+        
+        var maxIndex = 0
+        mapValues.forEach { if $0.count - 1 > maxIndex { maxIndex = $0.count - 1}}
+        var map = mapValues
+            .map { array in
+                var mutated = array
+                mutated.resize(maxIndex, fillWith: Character(" "))
+                return mutated
+            }
+            .transposed()
+            .filter { array in
+                guard let value = array.last,
+                      let _ = Int(String(value))
+                else
+                { return false}
+                return true
+            }
+            .map { Array($0.reversed()).filter({ $0 != " " }) }
+            .reduce(into: Stacks()) { (result,stack)  in
+                result[Int(String(stack.first!))] = Stack(stack.dropFirst())
             }
         
-        if filterDiagonals {
-            return vents.filter { $0.start.x == $0.end.x || $0.start.y == $0.end.y }
-        }
-        return vents
+        return (map, instructions)
     }
-    
-    private static func solve(_ input: [String], filter: Bool) -> Int {
-        var dic = [Position: Int]()
-        getVents(input, filterDiagonals: filter)
-        // get all positions
-            .map { $0.start.vector($0.end) }
-        // count up number of times hit that position
-            .forEach { positions in
-                for position in positions {
-                    dic[position, default: 0] += 1
-                }
-            }
-    
-        // count the positions where you hit it more than once
-        return dic.values.filter { $0 > 1 }.count
+}
+
+extension RangeReplaceableCollection {
+    public mutating func resize(_ size: IndexDistance, fillWith value: Iterator.Element) {
+        let c = count
+        if c < size {
+            append(contentsOf: repeatElement(value, count: c.distance(to: size)))
+        } else if c > size {
+            let newEnd = index(startIndex, offsetBy: size)
+            removeSubrange(newEnd ..< endIndex)
+        }
     }
 }
--- AdventOfCode/AdventOfCode/AdventOfCodeTests/2022/Day5Tests.swift
+++ AdventOfCode/AdventOfCode/AdventOfCodeTests/2022/Day5Tests.swift
@@ -3,25 +3,25 @@ import TwentyTwentyTwo
 
 class TestsDay5: XCTestCase {
 
-    let input = Array(Input.array(seperator: "\n", file: "Day5", compactmap:  { string in string } ).dropLast())
+    let input = Array(Input.array(seperator: "\n\n", file: "Day5", compactmap:  { string in string } ))
         
-    let example = Array(Input.array(seperator: "\n", file: "Day5Example", compactmap:  { string in string } ).dropLast())
+    let example = Array(Input.array(seperator: "\n\n", file: "Day5Example", compactmap:  { string in string } ))
     
     func test_day5_part1_example() {
-        XCTAssertEqual(Day5.part1(example), 5)
+        XCTAssertEqual(Day5.part1(example), "CMZ")
     }
     
     func test_day5_part1() {
-        XCTAssertEqual(Day5.part1(input), 6548)
+        XCTAssertEqual(Day5.part1(input), "CVCWCRTVQ")
     }
     
     func test_day5_example() throws {
-        XCTAssertEqual(try Day5.part2(example), 12)
+        XCTAssertEqual(try Day5.part2(example), "MCD")
     }
     
     func test_day5_part2() throws {
         measure {
-            XCTAssertEqual(try? Day5.part2(input), 19663)
+            XCTAssertEqual(try? Day5.part2(input), "CNSCZWLVT")
         }
     }
 }
